fe0a792922210cc7a66f8a8fa3c452d5023fbc32
Lars Kiesow <lkiesow@uos.de>
2026-03-10 11:01:16 +0100

    Initial commit

---
+++ zeusx-mobile.js
@@ -0,0 +1,146 @@
+console.info('UOSZEUSX ⋄ mobile');
+uosMobileRetries = 10;
+
+let injectCode = (event) => {
+  let status = 'unknown';
+  if (document.querySelector('img[title="Absent"]') || document.querySelector('img[title="Abwesend"]')) {
+    status = 'absent';
+  } else if (document.querySelector('img[title="Present"]') || document.querySelector('img[title="Anwesend"]')) {
+    status = 'present';
+  }
+
+  if (status === 'unknown' && uosMobileRetries > 0) {
+    // Check if we are on the internal login page
+    if (document.getElementById('uiLogOn_CD')) {
+      window.location.href = 'https://zeusx.uni-osnabrueck.de';
+    }
+
+    // Retry in a second
+    uosMobileRetries--;
+    console.warn('Status still unknown. Retries remaining:', uosMobileRetries);
+    window.setTimeout(injectCode, 1000);
+    return;
+  }
+
+  console.info('Status', status);
+
+  // Get today's netto work time
+  let workTime = document.querySelector('div.account-info-result div.account-list-element-value')?.innerText;
+  console.info('workTime', workTime);
+
+  // Get time toLocaleLowerCase();
+  // document.getElementById('uiBookingOverviewGridContainer')
+
+  let punchBtn = document.getElementById('TerminalButton0');
+  console.info('punchBtn', punchBtn);
+  let lastAction = document.getElementById('TerminalResult')?.querySelector('div[class="term-result-text"]')?.innerText;
+  console.info('lastAction', lastAction);
+
+  if (!document.getElementById('uos-mobile-style')) {
+    let head = document.getElementsByTagName('head')[0];
+
+    let style = document.createElement('style')
+    style.id = 'uos-mobile-style';
+    head.appendChild(style)
+    style.innerHTML = `
+      @media only screen and (max-width: 800px) {
+        #wrapper {
+          display: none;
+        }
+        #uos-mobile-wrapper {
+          diyplay: flex;
+          flex-direction: column;
+          justify-content: space-between;
+        }
+        #uos-mobile-wrapper > div {
+          margin: 0;
+          display: flex;
+          justify-content: center;
+          align-items: center;
+          width: 100%;
+          font-weight: bold;
+        }
+        #uos-mobile-status {
+          height: 40vh;
+          font-size: 42px;
+          color: white;
+        }
+        #uos-mobile-status.absent {
+          background-color: gray;
+        }
+        #uos-mobile-status.present {
+          background-color: green;
+        }
+        #uos-mobile-status.unknown {
+          background-color: red;
+        }
+        #uos-mobile-actions {
+          height: 50vh;
+        }
+        #uos-mobile-actions input {
+          width: 160px;
+          height: 160px;
+          border-radius: 80px;
+          border: none;
+          color: white;
+          background: black;
+        }
+      }
+      @media (min-width: 800px) {
+        #uos-mobile-wrapper {
+          display: none;
+        }
+      }`;
+
+    let script = document.createElement('script');
+    script.id = 'uos-mobile-script';
+    script.setAttribute('type', 'application/javascript');
+    head.appendChild(script)
+    script.innerHTML = `
+      function uosPunch() {
+        let punchBtn = document.getElementById('TerminalButton0');
+        punchBtn.click();
+        document.getElementById('uos-mobile-actions').innerText = 'Reloading in 3 seconds…';
+        window.setTimeout(uosReload, 3000);
+      }
+      function uosReload() {
+        window.location.href = 'https://zeusx.uni-osnabrueck.de';
+        //window.location.reload();
+      }`;
+
+    let meta = document.createElement('meta');
+    meta.setAttribute('name', 'viewport');
+    meta.setAttribute('content', 'width=device-width, initial-scale=1');
+    head.appendChild(meta);
+
+    let mobile = document.createElement('div');
+    mobile.id = 'uos-mobile-wrapper';
+    mobile.innerHTML = `
+        <div id='uos-mobile-status' class="${status}">${status}</div>
+        <div id='uos-mobile-actions'>
+          <input type=button onClick="uosPunch()" value="punch" />
+        </div>
+        <div id='uos-mobile-work-time'>
+          Netto work time today: ${workTime} h
+        </div>
+      `;
+    let body = document.getElementsByTagName('body')[0];
+    body.appendChild(mobile);
+  }
+
+  /*
+  let head = document.getElementsByTagName('head')[0].innerHTML;
+  document.getElementsByTagName('head')[0].innerHTML = head + `<style>
+    @media only screen and (max-width: 800px) {
+      #wrapper {
+        display: none;
+      }
+    }
+  </style>
+  <meta name=viewport content="width=device-width, initial-scale=1">`;
+  */
+};
+
+//window.addEventListener('load', injectCode);
+//window.setTimeout(injectCode, 250);
+window.setTimeout(injectCode, 100);
